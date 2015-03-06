c4f379db22c47164ec93dddf3eb9fbde0f6466d6
Viktor Lin <asfgjk123123@gmail.com>
2015-03-07 02:27:46 +0800

    first commit

---
+++ login.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+login_data = {
+'user':'',
+'pass':'',
+'Submit':'登入'
+}
+
+sess = requests.session()
+sess.get('https://ceiba.ntu.edu.tw/')
+sess.post('https://ceiba.ntu.edu.tw/ChkSessLib.php')
+sess.get('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php')
+r = sess.post('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php',data = login_data)
+sess.get(r.url)
+r = sess.get('https://ceiba.ntu.edu.tw/student/index.php?seme_op=all')
+
+soup = BeautifulSoup(r.content.decode('big5'))
+courses = soup.table.find_all(target='_blank')
+for idx,item in enumerate(courses):
+	print(idx+1,'.',item.string)
+go = int(input('選擇資料夾編號(輸入0返回上一頁):'))
+
+r = requests.get(courses[go-1]['href'])
+serial = r.url.split('=')[1]
+r = sess.get('https://ceiba.ntu.edu.tw/modules/index.php?csn='+serial+'&default_fun=hw&current_lang=chinese')
+r = sess.get('https://ceiba.ntu.edu.tw/modules/main.php?csn='+serial+'&default_fun=hw&current_lang=chinese')
+r = sess.get(r.url)
+print(r.content.decode('big5'))
+
+soup = BeautifulSoup(r.content.decode('big5'))
