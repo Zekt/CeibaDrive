23eec58017e303cea8fa9df0b42d9725da5882a1
Zekt <asfgjk123123@gmail.com>
2015-03-09 22:09:44 +0800

    can access file upload page

--- login.py
+++ login.py
@@ -1,24 +1,25 @@
-import requests
+import requests,getpass
 from bs4 import BeautifulSoup
 
 login_data = {
-'user':'',
-'pass':'',
-'Submit':'登入'
+    'user':input('請輸入帳號:'),
+    'pass':getpass.getpass('請輸入密碼:'),
+    'Submit':'登入'
 }
 
 sess = requests.session()
 sess.get('https://ceiba.ntu.edu.tw/')
 sess.post('https://ceiba.ntu.edu.tw/ChkSessLib.php')
 sess.get('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php')
-r = sess.post('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php',data = login_data)
+r = sess.post('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php',
+        data = login_data)
 sess.get(r.url)
 r = sess.get('https://ceiba.ntu.edu.tw/student/index.php?seme_op=all')
 
 soup = BeautifulSoup(r.content.decode('big5'))
 courses = soup.table.find_all(target='_blank')
 for idx,item in enumerate(courses):
-	print(idx+1,'.',item.string)
+    print(idx+1,'.',item.string)
 go = int(input('選擇資料夾編號(輸入0返回上一頁):'))
 
 r = requests.get(courses[go-1]['href'])
@@ -29,3 +30,26 @@ r = sess.get(r.url)
 print(r.content.decode('big5'))
 
 soup = BeautifulSoup(r.content.decode('big5'))
+hw = soup.find_all('tr')
+for idx,item in enumerate(hw):
+    print(idx+1,'.',item.td.string)
+go = int(input('選擇作業'))
+
+r = sess.get('https://ceiba.ntu.edu.tw/modules/hw/'+hw[go-1].td['href'])
+
+path = input('輸入上傳檔案路徑:')
+limit = input('自己的檔案大小上限自己設(?)(Bytes)')
+
+upload_data = {
+    'op':'hw_upload',
+    'hw_sn':'114530',
+    'old_file':'fjdsoia.cpp',
+    'MAX_FILE_SIZE':limit,
+    'file':'',
+    'Submit2':'確定並送出'
+}
+
+sess.post('https://ceiba.ntu.edu.tw/modules/hw/hw_show.php?current_lang=chinese',
+        data = upload_data,
+        files = path)
+
